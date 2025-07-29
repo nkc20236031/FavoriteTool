1c69659a27b16e7068cb4733a499d035e41dc1d6
mio <134259637+nkc20236031@users.noreply.github.com>
2025-07-29 13:49:53 +0900

    Add files via upload

---
+++ URLBrowserWindow/Editor/FavoriteURL.cs
@@ -0,0 +1,17 @@
+﻿#if UNITY_EDITOR
+namespace Editor.URLBrowserWindow
+{
+	[System.Serializable]
+	public class FavoriteURL
+	{
+		public string _name;
+		public string _url;
+        
+		public FavoriteURL(string name, string url)
+		{
+			_name = name;
+			_url = url;
+		}
+	}
+}
+#endif
