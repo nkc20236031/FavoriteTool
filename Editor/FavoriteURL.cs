262f0f45a62af5fd9a3cdc4380d699dfad4ac67a
mio <134259637+nkc20236031@users.noreply.github.com>
2025-09-19 15:22:07 +0900

    Add files via upload

---
+++ Editor/FavoriteURL.cs
@@ -0,0 +1,26 @@
+﻿#if UNITY_EDITOR
+namespace Editor.FavoriteTool
+{
+	[System.Serializable]
+	public class FavoriteURL
+	{
+		public string _name;
+		public string _url;
+		public FileType _fileType;
+        
+		public FavoriteURL(string name, string url)
+		{
+			_name = name;
+			_url = url;
+			_fileType = FileType.Browser; // デフォルトはブラウザ
+		}
+
+		public FavoriteURL(string name, string url, FileType fileType)
+		{
+			_name = name;
+			_url = url;
+			_fileType = fileType;
+		}
+	}
+}
+#endif
